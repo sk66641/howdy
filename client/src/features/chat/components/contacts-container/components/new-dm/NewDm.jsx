c4f7f135e8fbe7d613bb2f20a93a8391130fc24b
sk66641 <sanukumar66641@gmail.com>
2025-07-24 22:00:06 +0530

    Update NewDm.jsx

--- client/src/features/chat/components/contacts-container/components/new-dm/NewDm.jsx
+++ client/src/features/chat/components/contacts-container/components/new-dm/NewDm.jsx
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { colors, useDebounce } from '../../../../../../lib/utils'
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
-import { searchContactsAsync, selectContacts, setContactsEmpty, setCurrentChat, setChatType } from '../../../../chatSlice'
+import { searchContactsAsync, selectContacts, setContactsEmpty, setCurrentChat, setChatType, selectIsSearchingContacts } from '../../../../chatSlice'
 
 const NewDm = () => {
     const [searchQuery, setSearchQuery] = useState('');
@@ -14,6 +14,8 @@ const NewDm = () => {
     const [openNewContactModal, setOpenNewContactModal] = useState(false);
     const dispatch = useDispatch();
     const contacts = useSelector(selectContacts);
+    const isSearchingContacts = useSelector(selectIsSearchingContacts);
+
 
     const handleNewContact = () => {
         setOpenNewContactModal(false);
@@ -62,13 +64,21 @@ const NewDm = () => {
                         <DialogTitle>Please select a contact</DialogTitle>
                         <DialogDescription>howdy</DialogDescription>
                     </DialogHeader>
-                    <div>
+                    <div className='relative'>
                         <input
                             placeholder="Search Contacts"
                             className="rounded-lg p-3 bg-[#2c2e3b] border-none w-full"
                             value={searchQuery}
                             onChange={handleInputChange}
                         />
+                        {isSearchingContacts && (
+                            <div className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-400">
+                                <svg className="animate-spin h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                            </div>
+                        )}
                     </div>
                     <ScrollArea className="mt-3 h-[200px]">
                         <div className='flex flex-col justify-center'>
